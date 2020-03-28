d56a4a621f178e75b17f81f3941cfc870ea74838
Rick van Schijndel <rol3517@gmail.com>
2020-03-28 19:41:19 +0100

    Add script that removes MIC checks and replaces them with logging

--- scripts/remove-mic-checks.sh
+++ scripts/remove-mic-checks.sh
@@ -4,9 +4,5 @@
 # Useful for fuzzing without checksums.
 
 sed 's/return DECODE_FAIL_MIC;/if (decoder->verbose) { fprintf(stderr, "failed MIC"); }/g' src/devices/** -i
-
-# TODO: add 'return codes' to continue statements in rtl_433 and match/replace them here too.
-# example:
-#   continue; // DECODE_FAIL_MIC
-# This could then be replaced with a debug log statement instead.
+sed 's/continue; \/\/ DECODE_FAIL_MIC/if (decoder->verbose) { fprintf(stderr, "failed MIC"); }/g' src/devices/** -i
 
